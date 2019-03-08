f49164e0422cd932332e02b4eb73715170088651
Dennis Nikolay <dennis.nikolay@gmx.de>
2019-03-08 17:52:49 +0100

    added upload action to lib

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@
  * Created by rouven on 20.03.17.
  */
 
-import DataProvider from './components/DataProvider'
-import AppError from './components/AppError'
-import * as actionTypes from './actions/actionTypes'
-import * as actions from './actions'
-import ajaxErrorMiddleware from './middleware/ajaxErrorMiddleware'
-import dataReducer from './reducers/dataReducer'
-import queriesReducer from './reducers/queriesReducer'
-import errorReducer from './reducers/errorReducer'
-import userReducer from './reducers/userReducer'
+import DataProvider from './components/DataProvider';
+import AppError from './components/AppError';
+import * as actionTypes from './actions/actionTypes';
+import * as actions from './actions';
+import ajaxErrorMiddleware from './middleware/ajaxErrorMiddleware';
+import dataReducer from './reducers/dataReducer';
+import queriesReducer from './reducers/queriesReducer';
+import errorReducer from './reducers/errorReducer';
+import userReducer from './reducers/userReducer';
 
-const {loadData, download, submit, setUser, configureRequest, clearData, refreshData, confirmAndDelete, directDelete} = actions;
+const {loadData, download, submit, setUser, configureRequest, clearData, refreshData, confirmAndDelete, directDelete, upload} = actions;
 
 export default DataProvider;
 export {
@@ -33,5 +33,6 @@ export {
     clearData,
     refreshData,
     confirmAndDelete,
-    directDelete
+    directDelete,
+    upload
 };
